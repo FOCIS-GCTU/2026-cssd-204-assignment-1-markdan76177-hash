b0f2d9387be0aef70d57f519a67e1026375f4e4a
markdan76177-hash <markdan76177@gmail.com>
2026-05-31 15:08:57 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Enoch Kofi Sarpong
+# STUDENT ID:  2425404679
+# Email: 2425404679@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor,Enoch Kofi Sarpong , this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
+    #creating an empty line  using print but not variable
+    print()
+    #First print of the initials
+    print("...EKS")
+    print()
+    #printing my initIals in block letters EKS
+    print("...EEEEEEEEEEEE........KK........KK.........SSSSSSSSSSSS......")
+    print("...EE..................KK.......KK..........SS................")
+    print("...EE..................KK......KK...........SS................")
+    print("...EE..................KK.....KK............SS................")
+    print("...EEEEEEEEEEEE........KK....KK.............SSSSSSSSSSSS......")
+    print("...EE..................KK.....KK.......................SS.....")
+    print("...EE..................KK.......KK.....................SS.....")
+    print("...EE..................KK........KK....................SS.....")
+    print("...EE.............**...KK.........KK...**..............SS...**")
+    print("...EEEEEEEEEEEE...**...KK..........KK...**...SSSSSSSSSSSS...**")
+    print()
 
 main()
+
